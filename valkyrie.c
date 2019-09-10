5ac6a149493e5721a4b52c2f504a03fa2dc0b2c1
Joao Miguel Neves <joaoneves792@gmail.com>
2019-09-10 22:43:29 +0100

    fixed indentation

--- valkyrie.c
+++ valkyrie.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv){
 	prctl(PR_SET_CHILD_SUBREAPER, 1, NULL, NULL, NULL);
-	
+
 	pid_t forked_pid;
 	if(!(forked_pid = fork())){
 		setpgid(0, 0); // Place the forked process in a new group
@@ -19,7 +19,7 @@ int main(int argc, char** argv){
 	int returnStatus = -1;
 	pid_t terminated_pid = 0;
 	while(1){
-	       	while( (terminated_pid = waitpid(-1, &returnStatus, WNOHANG)) > 0 ){
+		while( (terminated_pid = waitpid(-1, &returnStatus, WNOHANG)) > 0 ){
 			if(terminated_pid == forked_pid){ //If we got the immediate child
 				kill(-(forked_pid), SIGKILL); //Atempt to kill all remaining grandchildren
 				sleep(5); //Make sure all grandchildren are reparented to us
